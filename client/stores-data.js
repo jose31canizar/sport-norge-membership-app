d3bc653e0a87e9d05026c84da26a549d168786b0
José Canizares <jose@Birgits-iMac-2.local>
2018-11-14 11:06:40 +0100

    scrape sport norge data

--- client/stores-data.js
+++ client/stores-data.js
@@ -39,97 +39,101 @@ export const data = [
     description:
       "Sport Norge Bodø åpnet 12.oktober 2017 i 2. etasje på Koch kjøpesenter. Vi kan tilby 400 kvadratmeter med sportsutstyr i lyse og moderne omgivelser. Her får du sportsutstyr fra mange av det største og mest kjente leverandørene i bransjen, som blant annet Osprey, Deuter, La Sportiva, Aclima, Fjellreven, Saucony og Bergans. Våre innkjøpere jobber hele tiden med å finne varer som er mest mulig tilpasset de lokale forholdene.",
     location: {
-      latitude: 0,
-      longitude: 0
+      latitude: 67.284166,
+      longitude: 14.379657
     },
-    street_address: ""
+    street_address: "Storgata 5 Koch-senteret 8006 Bodø"
   },
   {
     store: "Sport Norge Hunstad",
     categories: [],
-    image: "https://www.sportnorge.no/sites/default/files/butikk_1.jpg",
-    description: "En sportsbutikk på Knarvik for deg som liker å nyte Norge.",
+    image: "https://www.sportnorge.no/sites/default/files/hunstad.jpg",
+    description:
+      "Sport Norge Holding AS har sitt utspring fra Bergen. Høsten 2017 ble Sport Norge Hunstad etablert som et ledd av kjedens nasjonale satsning. Det er to butikker i Bodø, den andre ligger i sentrum.",
     location: {
-      latitude: 60.547892,
-      longitude: 5.285851
+      latitude: 67.259378,
+      longitude: 15.359997
     },
-    street_address: "Kvernhusmyrane 29"
+    street_address: "Hunstadsenteret Bodøveien 674"
   },
   {
     store: "Sport Norge Oslo",
     categories: [],
-    image: "https://www.sportnorge.no/sites/default/files/butikk_1.jpg",
-    description: "En sportsbutikk på Knarvik for deg som liker å nyte Norge.",
+    image: "https://www.sportnorge.no/sites/default/files/sportnorge-oslo.jpg",
+    description: "En sportsbutikk på Oslo for deg som liker å nyte Norge.",
     location: {
-      latitude: 60.547892,
-      longitude: 5.285851
+      latitude: 59.913838,
+      longitude: 10.751555
     },
-    street_address: "Kvernhusmyrane 29"
+    street_address: ""
   },
   {
     store: "Sport Norge Ålgård",
     categories: [],
-    image: "https://www.sportnorge.no/sites/default/files/butikk_1.jpg",
-    description: "En sportsbutikk på Knarvik for deg som liker å nyte Norge.",
+    image: "https://www.sportnorge.no/sites/default/files/magneten.jpg",
+    description: "En sportsbutikk på Ålgård for deg som liker å nyte Norge.",
     location: {
-      latitude: 60.547892,
-      longitude: 5.285851
+      latitude: 58.770042,
+      longitude: 5.84483
     },
-    street_address: "Kvernhusmyrane 29"
+    street_address: "Ole Nielsens Vei 25 4330"
   },
   {
     store: "Sport Norge Fyllingsdalen",
     categories: [],
-    image: "https://www.sportnorge.no/sites/default/files/butikk_1.jpg",
-    description: "En sportsbutikk på Knarvik for deg som liker å nyte Norge.",
+    image: "https://www.sportnorge.no/sites/default/files/fyllingsdalen.jpg",
+    description:
+      "Sport Norge Holding AS har eierskap i åtte Sport Norge-butikker i bergensområdet: Os, Laguneparken, Åsane, Fyllingsdalen, Knarvik, Askøy, Sotra og Sletten Senter.",
     location: {
-      latitude: 60.547892,
-      longitude: 5.285851
+      latitude: 60.349816,
+      longitude: 5.276672
     },
-    street_address: "Kvernhusmyrane 29"
+    street_address:
+      "Sport Norge Fyllingsdalen Krokatjønveien 11a 5147 Fyllingsdalen"
   },
   {
     store: "Sport Norge Laguneparken",
     categories: [],
-    image: "https://www.sportnorge.no/sites/default/files/butikk_1.jpg",
-    description: "En sportsbutikk på Knarvik for deg som liker å nyte Norge.",
+    image: "https://www.sportnorge.no/sites/default/files/laguneparken.jpg",
+    description:
+      "Sport Norge Laguneparken er en sportsbutikk for deg som liker å nyte Norge.",
     location: {
-      latitude: 60.547892,
-      longitude: 5.285851
+      latitude: 60.294066,
+      longitude: 5.328414
     },
-    street_address: "Kvernhusmyrane 29"
+    street_address: "Laguneveien 21 5239 Rådal"
   },
   {
     store: "Sport Norge Askøy",
     categories: [],
-    image: "https://www.sportnorge.no/sites/default/files/butikk_1.jpg",
+    image: "https://www.sportnorge.no/sites/default/files/askoy.jpg",
     description: "En sportsbutikk på Knarvik for deg som liker å nyte Norge.",
     location: {
-      latitude: 60.547892,
-      longitude: 5.285851
+      latitude: 60.422352,
+      longitude: 5.203002
     },
-    street_address: "Kvernhusmyrane 29"
+    street_address: "Sport Norge Askøy Kleppevegen 110 5308 Kleppestø"
   },
   {
     store: "Sport Norge Sotra",
     categories: [],
-    image: "https://www.sportnorge.no/sites/default/files/butikk_1.jpg",
-    description: "En sportsbutikk på Knarvik for deg som liker å nyte Norge.",
+    image: "https://www.sportnorge.no/sites/default/files/sotra.jpg",
+    description: "En sportsbutikk på Sotra, for deg som liker å nyte Norge.",
     location: {
-      latitude: 60.547892,
-      longitude: 5.285851
+      latitude: 60.358865,
+      longitude: 5.12507
     },
-    street_address: "Kvernhusmyrane 29"
+    street_address: "Sartor Storsenter, Straume"
   },
   {
     store: "Sport Norge Sletten",
     categories: [],
-    image: "https://www.sportnorge.no/sites/default/files/butikk_1.jpg",
-    description: "En sportsbutikk på Knarvik for deg som liker å nyte Norge.",
+    image: "https://www.sportnorge.no/sites/default/files/sletten.jpg",
+    description: "En sportsbutikk på Sletten, for deg som liker å nyte Norge.",
     location: {
-      latitude: 60.547892,
-      longitude: 5.285851
+      latitude: 60.356396,
+      longitude: 5.359341
     },
-    street_address: "Kvernhusmyrane 29"
+    street_address: "Sletten Senter Vilhelm Bjerknes' vei 4 5081 Bergen"
   }
 ];
